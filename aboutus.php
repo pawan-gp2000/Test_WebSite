ff45d419476f025bd28287703d3b74950afb0a08
pawan-gp2000 <pawan_gp2000@yahoo.co.in>
2012-10-07 13:54:25 -0400

    commit

---
+++ aboutus.php
@@ -0,0 +1,65 @@
+<?php
+include_once 'header/header.php';
+?>
+<!--  banner images    -->
+<div id="banner" style="float: left;">
+	<img src="images/website_design.jpg" height="205" width="900">
+</div>
+<!-- div banner close -->
+
+<!-- Content in the web site is here -->
+<div class="container">
+	<div class="span12">
+		<section id="ios">
+		<h1>About Us</h1>
+			<p class="lead">Daa Systems offers complete, end-to-end services for
+				mobile application development across all major platforms iOS
+				devices, android OS, RIM os, Ubuntu MID as well as Windows Mobile OS
+				8 or higher.Expertise and Tools</p>
+			<h4>MOBILE STRATEGY</h4>
+			<p>Mobile development is not just about the developing couple of
+				mobile application. It is the Attitude towards result oriented work
+				process. Our experience and knowledge of mobile technology coupled
+				with in-depth research of your area and the passion to excel ensures
+				that we deliver "what is exactly needed" strategies for your
+				business.</p>
+			<h4>DESIGN</h4>
+			<p>"Design it the process ending with the result", Design is one of
+				key determinants of an application's success. Nothing beats an
+				aesthetically pleasing UI that delivers a smooth flowing user
+				experience. Our design team is compiled of creative technical
+				artists who can think out of the box to come up with trend-setting
+				and unique design patterns while maintaining a natural user
+				experience.</p>
+			<h4>PROJECT MANAGEMENT</h4>
+			<p>In pursuit of creativity and innovation we have not compromised on
+				organizational requirements. We have a highly efficient project
+				management system in place that helps keep track of every minute
+				progress of all the projects.</p>
+			<h4>QUALITY ASSURANCE</h4>
+			<p>Quality is of essence in the highly competitive app development
+				space. We have a specialized in-house QA team that conducts rigorous
+				review and testing to ensure every app we develop is bug-free and
+				runs smoothly. Our QA initiatives are not just geared towards
+				detecting problems but also to come up with precise solution and
+				improvement suggestions for the overall betterment of the app.</p>
+			<h4>DEPLOYMENT AND MAINTENANCE</h4>
+			<p>App deployment is a delicate process where a single error can get
+				your app rejected. That's why; we provide specialized services in
+				app deployment wherein our experienced deployment experts will
+				assist you in deploying your app. We also offer customized app
+				maintenance programs to ensure that your app is updated on-time and
+				runs well on all intended devices and platforms.</p>
+		</section>
+	</div>
+</div>
+
+<?php 
+include_once 'footer/footer.php';
+?>
+
+
+</div>
+<!-- master div close -->
+</body>
+</html>
