35e5dfff50b3729209405344bbe16825e87e4b6f
pawan-gp2000 <pawan_gp2000@yahoo.co.in>
2012-10-17 20:45:15 +0530

    New DNews Page added

---
+++ dnews.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>DNews</title>
+<?php
+include_once 'header/header.php';
+?>
+
+<!--  banner images    -->
+<div id="banner" style="float:left;">
+	<img src="images/news_banner_2.png" />
+</div>
+<!-- div banner close -->
+<!-- site content start -->
+
+<!-- Content in the web site is here -->
+
+ <div id="Content" style="background-color: #FFFFFF; width: 900px; clear: both;" >
+
+
+	<div id="main_text" style="width: 900px; padding: 5px; clear:both;">
+		<center><h1>D News</h1></center>
+      <p>Daa Systems created Dnews application to follow in the foot steps of Anroid community with "open source". Any one can follow any event around the world as it unfolds. From Middle East to Australia and from USA to Afganistan you have the power to watch what is happening.
+All the news provided is based on BBC Feeds.
+	</div>
+	<!-- div main_text close -->
+    <center>
+    <div id="img" style="background-color:#EAEAEA; width:900px; clear:both; clear:right; " >
+
+        <div id="first_img" style="width:425px; height:280px; float:left; padding-right:5px;">
+          <img src="images/dnews/news_1.png">
+        </div>
+
+        <div id="second_img" style="width:425px; height:280px; float:right; ">
+          <img src="images/dnews/news_2.png">
+        </div>
+
+        <div id="third_img" style="width:425px; height:280px; float:left; padding-right:5px; ">
+          <img src="images/dnews/open_news.png">
+        </div>
+
+      <div id="fourht_img" style="width:425px; height:280px; float:right; ">
+          <img src="images/dnews/cat.png">
+      </div>
+
+    <div id="google play" style="clear:both; float:none; width:250px; height:80px; ">
+    
+        <a href="https://play.google.com/store/apps/details?id=com.daa.news&feature=search_result#?t=W251bGwsMSwyLDEsImNvbS5kYWEubmV3cyJd" ><img src="images/googleplay.png" alt="Link to Google Play" width="228" height="76" ></a>
+ 
+    </div>
+  
+
+        </div> <!-- div IMG close -->
+ </center>
+ 
+ </div><!-- site content close -->
+
+ 
+	<?php 
+	include_once 'footer/footer.php';
+	?>
+</div>
+<!-- master div close -->
+</body>
+</html>
