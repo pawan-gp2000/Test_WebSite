20fdc19ececcc2c48b141bd88ea6e2224dc1acf0
pawan-gp2000 <pawan_gp2000@yahoo.co.in>
2012-10-17 20:52:27 +0530

    Improve copyright text and color of footer. Use it as div for modular functionality

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+<!-- footer starts from here -->
+
+	    <footer>
+    	<div id="copyright"; style="width:900px; height:35px; float:right; line-height:30px">
+    	 <p>  Copyright 1999-2012 by Daa Systems. All Rights Reserved.
+    	 </p>
+    	</div><!-- copyright div close -->
+        </footer> 
+	
+	  <!--footer end from here -->
+
